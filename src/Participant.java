91c84f6d48fa05fa5d3929f11d7e9a1f39632da2
Mathumitha-create <mathumitha.s2024cse@sece.ac.in>
2025-09-20 20:40:23 +0530

    Update Participant.java

--- src/Participant.java
+++ src/Participant.java
@@ -1,10 +1,24 @@
+// Participant class - represents a user who takes part in an event
+// Extends the User class (inheritance concept in Java)
+
 public class Participant extends User {
+
+    // constructor to initialize participant details
     public Participant(String name, String email) {
+        // calling parent class constructor
         super(name, email);
     }
 
+    // overriding method to show participant details
     @Override
     public void displayDetails() {
-        System.out.println("Participant -> Name: " + name + ", Email: " + email);
+        System.out.println("---- Participant Details ----");
+        System.out.println("Name  : " + name);
+        System.out.println("Email : " + email);
+    }
+
+    // extra method (student style - makes class more useful)
+    public void registerForEvent(Event e) {
+        System.out.println(name + " has registered for the event: " + e.getEventName());
     }
 }
