c62cfd8c533a837c4d703e918924717375f94a02
Mathumitha-create <mathumitha.s2024cse@sece.ac.in>
2025-09-21 15:09:41 +0530

    Add User class with name and email fields, including displayDetails() method

--- src/User.java
+++ src/User.java
@@ -1,12 +1,19 @@
+// Represents a general user with a name and email, and includes a method to display user information
 public class User {
+
+    // The user's full name
     protected String name;
+
+    // The user's email address
     protected String email;
 
+    // Constructor to create a new User with a specified name and email
     public User(String name, String email) {
         this.name = name;
         this.email = email;
     }
 
+    // Prints the user's details to the console
     public void displayDetails() {
         System.out.println("Name: " + name + ", Email: " + email);
     }
